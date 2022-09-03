d1347da95847c05f42bba6d47ee0c9bdb10c3b9e
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2022-09-03 10:31:38 -0300

    Atualizacao dos projetos

--- web/AppElberth/src/main/java/br/edu/infnet/controller/AlunoController.java
+++ web/AppElberth/src/main/java/br/edu/infnet/controller/AlunoController.java
@@ -1,6 +1,9 @@
 package br.edu.infnet.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +15,10 @@ import br.edu.infnet.model.domain.Aluno;
 public class AlunoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public AlunoController() {
+	private List<Aluno> alunos;
+
+	public AlunoController() {
+		alunos = new ArrayList<Aluno>();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -21,23 +27,37 @@ public class AlunoController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		Aluno aluno = new Aluno(request.getParameter("nome"), request.getParameter("email"));		
+		Aluno aluno = new Aluno(request.getParameter("nome"), request.getParameter("email"));
+		aluno.setIdade(Integer.valueOf(request.getParameter("idade")));
+		aluno.setMensalidade(Float.valueOf(request.getParameter("mensalidade")));
 		aluno.setCurso(request.getParameter("curso"));
 		aluno.setDisciplinas(request.getParameterValues("disciplinas"));
 		aluno.setRegiao(request.getParameter("regiao"));
 		
-		System.out.println(aluno);
+		alunos.add(aluno);
 		
-		request.getRequestDispatcher("confirmacao.html").forward(request, response);		
+		PrintWriter out = response.getWriter();		
+		out.println(aluno);
+		out.println("<h2>Total de alunos cadastrados = "+alunos.size()+"</h2>");
 
-		//TODO Criar o método de impressão para chamar o tostring e o foreach
-//		if(qtde > 0) {
-//			System.out.println("Disciplinas:");
-//			for(String disciplina : disciplinas) {
-//				System.out.println("-" + disciplina);
-//			}
-//		} else {
-//			System.out.println("Não existem disciplinas selecionadas");
-//		}
+		for(Aluno a : alunos) {
+			out.println("<h3>"+a.getNome()+" - "+a.getEmail()+" - "+a.getIdade()+"</h3>");
+		}
+
+		out.println("<a href='aluno'>Voltar</a>");		
 	}
 }
+
+
+
+
+//request.getRequestDispatcher("confirmacao.html").forward(request, response);		
+//TODO Criar o método de impressão para chamar o tostring e o foreach
+//if(qtde > 0) {
+//	System.out.println("Disciplinas:");
+//	for(String disciplina : disciplinas) {
+//		System.out.println("-" + disciplina);
+//	}
+//} else {
+//	System.out.println("Não existem disciplinas selecionadas");
+//}
--- web/AppElberth/src/main/java/br/edu/infnet/model/domain/Aluno.java
+++ web/AppElberth/src/main/java/br/edu/infnet/model/domain/Aluno.java
@@ -2,6 +2,8 @@ package br.edu.infnet.model.domain;
 
 public class Aluno {
 	private String nome;
+	private int idade;
+	private float mensalidade;
 	private String email;
 	private String curso;
 	private String[] disciplinas;
@@ -17,21 +19,67 @@ public class Aluno {
 		
 		int qtde = disciplinas == null ? 0 : disciplinas.length;
 
-		return String.format("[%s] O aluno %s (%s) da região %s está inscrito em %d disciplinas!\n", 
+		return String.format("[%s] O aluno %s (%s) da região %s tem %d anos, paga R$%.2f e está inscrito em %d disciplinas!\n", 
 				curso,
 				nome,
 				email,
 				regiao,
+				idade,
+				mensalidade,
 				qtde				
 			);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public float getMensalidade() {
+		return mensalidade;
+	}
+
+	public void setMensalidade(float mensalidade) {
+		this.mensalidade = mensalidade;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCurso() {
+		return curso;
 	}
 
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
 
+	public String[] getDisciplinas() {
+		return disciplinas;
+	}
+
 	public void setDisciplinas(String[] disciplinas) {
 		this.disciplinas = disciplinas;
+	}
+
+	public String getRegiao() {
+		return regiao;
 	}
 
 	public void setRegiao(String regiao) {
